cf11dabc40f231ede7163276d7b6adbd8ff70d8c
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-10-08 16:45:03 -0700

    add get and post routes for delete.cshtml

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -47,5 +47,20 @@ namespace HairSalon.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult Delete(int id)
+    {
+      Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult Delete(int id)
+    {
+      Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistID == id);
+      _db.Stylist.Remove(thisSylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
